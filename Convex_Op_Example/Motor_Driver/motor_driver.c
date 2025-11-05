5f2f7180ba11c5e094aaf04354bbf0acf4b0417d
iek2443 <142242839+iek2443@users.noreply.github.com>
2025-11-05 14:47:52 +0300

    Update motor_driver.c
    Bug fixed.

--- Convex_Op_Example/Motor_Driver/motor_driver.c
+++ Convex_Op_Example/Motor_Driver/motor_driver.c
@@ -13,21 +13,21 @@ void Start_Motor(uint32_t speed, motor_direction_t direction) {
 
 	if (direction == MOTOR_LEFT) {
 		HAL_GPIO_WritePin(MOTOR_IN1_GPIO_Port, MOTOR_IN1_Pin, Motor_Set_Pin);
-		HAL_GPIO_WritePin(MOTOR_IN2_GPIO_Port, MOTOR_IN1_Pin, Motor_Reset_PIN);
+		HAL_GPIO_WritePin(MOTOR_IN2_GPIO_Port, MOTOR_IN2_Pin, Motor_Reset_PIN);
 
 	} else {
 		HAL_GPIO_WritePin(MOTOR_IN1_GPIO_Port, MOTOR_IN1_Pin, Motor_Reset_PIN);
-		HAL_GPIO_WritePin(MOTOR_IN2_GPIO_Port, MOTOR_IN1_Pin, Motor_Set_Pin);
+		HAL_GPIO_WritePin(MOTOR_IN2_GPIO_Port, MOTOR_IN2_Pin, Motor_Set_Pin);
 	}
 
 #else
 	if (direction == MOTOR_LEFT) {
 		HAL_GPIO_WritePin(MOTOR_IN1_GPIO_Port, MOTOR_IN1_Pin, Motor_Reset_PIN);
-		HAL_GPIO_WritePin(MOTOR_IN2_GPIO_Port, MOTOR_IN1_Pin, Motor_Set_Pin);
+		HAL_GPIO_WritePin(MOTOR_IN2_GPIO_Port, MOTOR_IN2_Pin, Motor_Set_Pin);
 
 	} else {
 		HAL_GPIO_WritePin(MOTOR_IN1_GPIO_Port, MOTOR_IN1_Pin, Motor_Set_Pin);
-		HAL_GPIO_WritePin(MOTOR_IN2_GPIO_Port, MOTOR_IN1_Pin, Motor_Reset_PIN);
+		HAL_GPIO_WritePin(MOTOR_IN2_GPIO_Port, MOTOR_IN2_Pin, Motor_Reset_PIN);
 	}
 
 #endif
@@ -49,3 +49,4 @@ void Stop_Motor() {
 	__HAL_TIM_SET_COMPARE(&MOTOR_HTIM, MOTOR_TIM_CHANNEL, 0);
 	HAL_TIM_PWM_Stop(&MOTOR_HTIM, MOTOR_TIM_CHANNEL);
 }
+
